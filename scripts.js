1fe7c0c5ec034daff79635b7ca01e011a46c4c47
DMBegala <DMBegala@users.noreply.github.com>
2020-04-17 17:45:15 +0200

    Update scripts.js
    Arrays
    For Loops
    Functions

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ console.log("JavaScript is awesome")
 
 let colors = ["green", "blue", "red"]
 
+
+
 // "post" is an object
 
 let post =  {
@@ -17,6 +19,9 @@ let post =  {
 console.log(colors);
 console.log(post);
 
+
+// if statements
+
 let age = 16;
 
 if (age >= 18) {
@@ -25,6 +30,17 @@ if (age >= 18) {
     console.log("You're a kid!");
 }
 
+
+// For Loop section
+
+for (let i = 0;  i < 10; i++) {
+    console.log(i);
+}
+
+
+console.log("This was a for-loop");
+
+console.log("This is another for-loop");
 
 let printValues = (array) => {
          let arrayLength = array.length
@@ -36,4 +52,29 @@ let printValues = (array) => {
   printValues([0, 3, 6, 7, 9]);
   
 
-console.log("The journey just begins");
+// ez egy function
+colors.forEach(color =>  {
+    console.log(color);
+});
+
+// ez egy másik function, első része az input/paraméter 
+// f(x) = x * x
+let myFunction = x =>  {
+    console.log(x);
+};
+
+let greeter = name  => {
+    console.log(`Hello, ${name}`);
+    if (name === "Adam") {
+        console.log("He is an adult");
+    }
+}
+greeter("Adam");
+greeter("Daniel");
+greeter("Mark");
+greeter("Marcell");
+greeter("Judith");
+
+
+
+console.log("This is the end of code. See you in the matrix");
